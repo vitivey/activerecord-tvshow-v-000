a8d874fb7d88d3a7260674427a41b2455a7e5a53
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 17:32:09 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,30 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.where("rating = ?", Show.highest_rating).first
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    self.all.order(:name)
+  end
+end

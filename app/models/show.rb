cc0be5ce283eccce77c2e9c56cd9025efefc8958
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 17:31:45 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -20,4 +20,11 @@ class Show < ActiveRecord::Base
     self.sum(:rating)
   end
 
+  def self.popular_shows
+    self.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    self.all.order
+  end
 end
